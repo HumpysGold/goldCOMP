4679b21b71d53876ddff96544d850277678b5095
SHAKOTN <shakotn@protonmail.com>
2023-12-21 18:15:37 +0200

    test: add more tests for goldCOMP; refactor code locations

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -25,4 +25,67 @@ contract BaseFixture is Test {
 
         gComp = new goldCOMP();
     }
+
+    ///////////////////////////// Invariants ///////////////////////////////
+
+    function invariant_deposit(uint256 _amount) internal {
+        // assert invariants (erc20 storage + internal)
+        assertEq(gComp.totalSupply(), _amount);
+        assertEq(gComp.totalAssetBalance(), _amount);
+        assertEq(COMP.balanceOf(address(gComp)), _amount);
+        assertEq(gComp.internalAssetBalances(COMP_DEPOSITOR_AGENT), _amount);
+    }
+
+    function invariant_queueWithdraw(
+        uint256 _prevInternalBalance,
+        uint256 _queueAmount,
+        uint256 _queueIndex
+    )
+        internal
+    {
+        (uint256 queueAmount, uint256 timestamp, uint256 releaseTimestamp, bool withdrawn) =
+            gComp.queuedWithdrawals(COMP_DEPOSITOR_AGENT, _queueIndex);
+
+        // assert invariants (internal storage + mapping)
+        assertEq(gComp.internalAssetBalances(COMP_DEPOSITOR_AGENT), _prevInternalBalance - _queueAmount);
+        assertEq(queueAmount, _queueAmount);
+        assertEq(timestamp, block.timestamp);
+        assertEq(releaseTimestamp, block.timestamp + gComp.daysToWait());
+        assertFalse(withdrawn);
+    }
+
+    function invariant_completeWithdraw(
+        uint256 _index,
+        uint256 _gCompSupplyBefore,
+        uint256 _gCompBalanceBefore,
+        uint256 _compBalanceBefore,
+        uint256 _withdrawAmount
+    )
+        internal
+    {
+        (,,, bool withdrawn) = gComp.queuedWithdrawals(COMP_DEPOSITOR_AGENT, _index);
+
+        // assert invariants
+        assertEq(gComp.totalSupply(), _gCompSupplyBefore - _withdrawAmount);
+        assertEq(gComp.balanceOf(COMP_DEPOSITOR_AGENT), _gCompBalanceBefore - _withdrawAmount);
+        assertEq(COMP.balanceOf(COMP_DEPOSITOR_AGENT), _compBalanceBefore + _withdrawAmount);
+        assertTrue(withdrawn);
+    }
+
+    ///////////////////////////// Helpers ///////////////////////////////
+
+    function userDeposit(uint256 amount) internal {
+        // positive amounts
+        vm.assume(amount > 0);
+        // currently 6.8m in circ
+        vm.assume(amount < 6_000_000e18);
+
+        deal(address(COMP), COMP_DEPOSITOR_AGENT, amount);
+
+        // approve + deposit
+        vm.prank(COMP_DEPOSITOR_AGENT);
+        COMP.approve(address(gComp), amount);
+        vm.prank(COMP_DEPOSITOR_AGENT);
+        gComp.deposit(amount);
+    }
 }
--- test/goldCOMP.t.sol
+++ test/goldCOMP.t.sol
@@ -14,6 +14,31 @@ contract goldCOMPTest is BaseFixture {
         invariant_deposit(amount);
     }
 
+    /// @dev Case when user queues a lot of withdrawals and able to withdraw all of them at once
+    function testDepositWithdrawMultipleWithdrawals(uint256 amount, uint256 numDeposits) public {
+        vm.assume(numDeposits > 0 && numDeposits < 5);
+        for (uint256 i = 0; i < numDeposits; i++) {
+            userDeposit(amount);
+        }
+        // Queue all withdrawals
+        for (uint256 i = 0; i < numDeposits; i++) {
+            vm.prank(COMP_DEPOSITOR_AGENT);
+            gComp.queueWithdraw(amount);
+        }
+        uint256 compBalancePrev = COMP.balanceOf(COMP_DEPOSITOR_AGENT);
+        uint256 gCompSupplyPrev = gComp.totalSupply();
+        uint256 gCompBalancePrev = gComp.balanceOf(COMP_DEPOSITOR_AGENT);
+        skip(gComp.daysToWait());
+        // Withdraw everything now
+        for (uint256 i = 0; i < numDeposits; i++) {
+            vm.prank(COMP_DEPOSITOR_AGENT);
+            gComp.withdraw();
+        }
+
+        // Make sure everything is withdrawn
+        invariant_completeWithdraw(0, gCompSupplyPrev, gCompBalancePrev, compBalancePrev, amount * numDeposits);
+    }
+
     function testDeposit_revert() public {
         // zero amount should revert
         vm.prank(address(45));
@@ -96,68 +121,5 @@ contract goldCOMPTest is BaseFixture {
         vm.prank(address(4_343_435_545));
         vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(4_343_435_545)));
         gComp.setDaysToWait(daysToWaitNewVal);
-    }
-
-    ///////////////////////////// Invariants ///////////////////////////////
-
-    function invariant_deposit(uint256 _amount) internal {
-        // assert invariants (erc20 storage + internal)
-        assertEq(gComp.totalSupply(), _amount);
-        assertEq(gComp.totalAssetBalance(), _amount);
-        assertEq(COMP.balanceOf(address(gComp)), _amount);
-        assertEq(gComp.internalAssetBalances(COMP_DEPOSITOR_AGENT), _amount);
-    }
-
-    function invariant_queueWithdraw(
-        uint256 _prevInternalBalance,
-        uint256 _queueAmount,
-        uint256 _queueIndex
-    )
-        internal
-    {
-        (uint256 queueAmount, uint256 timestamp, uint256 releaseTimestamp, bool withdrawn) =
-            gComp.queuedWithdrawals(COMP_DEPOSITOR_AGENT, _queueIndex);
-
-        // assert invariants (internal storage + mapping)
-        assertEq(gComp.internalAssetBalances(COMP_DEPOSITOR_AGENT), _prevInternalBalance - _queueAmount);
-        assertEq(queueAmount, _queueAmount);
-        assertEq(timestamp, block.timestamp);
-        assertEq(releaseTimestamp, block.timestamp + gComp.daysToWait());
-        assertFalse(withdrawn);
-    }
-
-    function invariant_completeWithdraw(
-        uint256 _index,
-        uint256 _gCompSupplyBefore,
-        uint256 _gCompBalanceBefore,
-        uint256 _compBalanceBefore,
-        uint256 _withdrawAmount
-    )
-        internal
-    {
-        (,,, bool withdrawn) = gComp.queuedWithdrawals(COMP_DEPOSITOR_AGENT, _index);
-
-        // assert invariants
-        assertEq(gComp.totalSupply(), _gCompSupplyBefore - _withdrawAmount);
-        assertEq(gComp.balanceOf(COMP_DEPOSITOR_AGENT), _gCompBalanceBefore - _withdrawAmount);
-        assertEq(COMP.balanceOf(COMP_DEPOSITOR_AGENT), _compBalanceBefore + _withdrawAmount);
-        assertTrue(withdrawn);
-    }
-
-    ///////////////////////////// Helpers ///////////////////////////////
-
-    function userDeposit(uint256 amount) internal {
-        // positive amounts
-        vm.assume(amount > 0);
-        // currently 6.8m in circ
-        vm.assume(amount < 6_000_000e18);
-
-        deal(address(COMP), COMP_DEPOSITOR_AGENT, amount);
-
-        // approve + deposit
-        vm.prank(COMP_DEPOSITOR_AGENT);
-        COMP.approve(address(gComp), amount);
-        vm.prank(COMP_DEPOSITOR_AGENT);
-        gComp.deposit(amount);
     }
 }
