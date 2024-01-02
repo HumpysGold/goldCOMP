5066436c13e1f5e362a728dffb5c10b9b266e81c
Kapusniak <goldkapusniak@proton.me>
2024-01-02 22:21:32 +0100

    test: delegation & delegatee casting vote in bravo

--- src/interfaces/IBravoGovernance.sol
+++ src/interfaces/IBravoGovernance.sol
@@ -3,7 +3,36 @@
 pragma solidity 0.8.20;
 
 interface IBravoGovernance {
+    struct Receipt {
+        bool hasVoted;
+        uint8 support;
+        uint96 votes;
+    }
+
     event VoteCast(address indexed voter, uint256 proposalId, uint8 support, uint256 votes, string reason);
+    event ProposalCreated(
+        uint256 id,
+        address proposer,
+        address[] targets,
+        uint256[] values,
+        string[] signatures,
+        bytes[] calldatas,
+        uint256 startBlock,
+        uint256 endBlock,
+        string description
+    );
 
     function castVote(uint256 proposalId, uint8 support) external;
+
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        string[] memory signatures,
+        bytes[] memory calldatas,
+        string memory description
+    )
+        external
+        returns (uint256);
+
+    function getReceipt(uint256 proposalId, address voter) external view returns (Receipt memory);
 }
